aec0415827772bf098d44ac77a53cd365f674394
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-28 22:00:46 -0400

    algo problem pseudocode

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -4,6 +4,9 @@ function findFirstDuplicate(arr) {
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: a");
+  console.log("=>", findFirstDuplicate([a, b, c, d, a]));
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -16,4 +19,13 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+/*
+1)Iterate through the array
+2)Check the value of the first index and compare it to the rest of the array for a match. 
+3)If there is a match optput the match. 
+4)If no match move on to the next index and comapre it to the rest of the array. 
+5)Keep going until all values in the array have been checked. 
+If not match is found return -1. 
+*/
+
 // And a written explanation of your solution
