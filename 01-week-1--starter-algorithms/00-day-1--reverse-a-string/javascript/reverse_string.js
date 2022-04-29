74747d95c43cfd469084e6372744509451ca7991
Doug Jones <DSJR1284@GMAIL.COM>
2022-04-28 21:44:33 -0400

    js algo problem solved

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,12 +1,10 @@
 function reverseString(str) {
   // type your code here
-  let reversed = "";
-
-  for (let i = str.length - 1; i > -1; --i) {
-    reversed = reversed + str[i];
-  }
-
-  return reversed;
+  let reversed = "";      
+  for (var i = str.length - 1; i >= 0; i--){         
+    reversed += str[i];  
+  }     
+return reversed;  
 }
 
 if (require.main === module) {
@@ -23,4 +21,17 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+/*
+1)Take in a string 
+2)Check the length of the string. 
+3)Reverse the string 
+4)Print it out 
+*/ 
+
 // And a written explanation of your solution
+/* 
+Set reverse to a empty string 
+Create a for loop with i set to str.length -1 
+if i is greater than or equal to 0 set i to decriment 
+return the reversed string. 
+*/
