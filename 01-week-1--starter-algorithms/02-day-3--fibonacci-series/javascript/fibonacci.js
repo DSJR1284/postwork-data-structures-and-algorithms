98d2ebde6e073de6c1141665dd0d692128dcdd97
Doug Jones <DSJR1284@GMAIL.COM>
2022-05-22 23:44:05 -0400

    test solution

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
   // type your code here
+  let a = 0, b = 1, val;
+
+  while (num >= 0){
+    val = a;
+    a = a + b;
+    b = val;
+    num--;
+  }
+
+  return b;
 }
 
 // Take a sequence of of numbers starting with 0 followed by 1. Add the next two number in line to get the next number in the sequence. 
@@ -24,6 +34,11 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+/* 
+1)Loop through the sequence and add the first two numbers together.
+2) Take the sum of of the loop and add the next two numbers to get the next number in the sequence. 
+*/
+
 
 
 
